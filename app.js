f1f2bb8b993d221a4a76f0080fdb816cf837e0de
Matías Juan Ariaudo <matiariaudo@gmail.com>
2022-12-15 14:57:45 -0500

    Add wsp

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+const { Client , LocalAuth ,MessageMedia} = require('whatsapp-web.js');
 const express = require('express')
-
 const app = express()
 require('dotenv').config()
+let qri="NO";
+
+client = new Client({authStrategy: new LocalAuth({ clientId: "Youtube" })});
+client.on('qr', (qr) => {console.log(qr);
+    qri=qr;
+});
+
 
 app.use(express.json())
 
-app.get('/:valor', function (req, res) {
-    res.json(req.params.valor)
+app.get('*', function (req, res) {
+    res.json(qri)
 });
 app.listen(process.env.PORT)
