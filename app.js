6d993dd47c962e7949798ca29fffcd773e35c189
Matías Juan Ariaudo <matiariaudo@gmail.com>
2022-12-15 18:37:41 -0500

    Add wspAudio

--- app.js
+++ app.js
@@ -2,20 +2,37 @@ const { Client , LocalAuth ,MessageMedia} = require('whatsapp-web.js');
 var randomstring = require("randomstring");
 const express = require('express')
 const app = express()
+const yt = require("yt-converter");
 require('dotenv').config()
+
 let qri="NO";
 
-client = new Client({authStrategy: new LocalAuth({ clientId: "Youtube" })});
-client.on('qr', (qr) => {console.log(qr);
-    qri=qr;
-});
+function descargar_video(url,newName)
+{
+    return new Promise((resolve, reject) => {
+        yt.convertAudio({
+            url: url,
+            itag: 140,
+            directoryDownload: __dirname+"/downloads",
+            title: newName
+            }, (a)=>{
+            console.log("Bajando: %",a)
+            }, ()=>{
+            console.log("Se ha bajado")
+            resolve(true)
+        })       
+    })
+    
+}
 
 
 app.use(express.json())
 
-app.get('*', function (req, res) {
-    res.json(randomstring.generate(7) )
+app.get('*', async(req, res)=>{
+    await descargar_video("https://www.youtube.com/watch?v=azdwsXLmrHEhttps://www.youtube.com/watch?v=azdwsXLmrHE","azdwsXLmrHE")
+    res.json(randomstring.generate(7)+" se bajo azdwsXLmrHE" )
 });
+
 console.log("App corriendo en ",process.env.PORT)
 app.listen(process.env.PORT)
 
